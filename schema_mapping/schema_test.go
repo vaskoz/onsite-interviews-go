e1978b3b4fee875d8179024893452da6350d51ca
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-09-08 16:22:53 -0600

    add another testcase

--- schema_mapping/schema_test.go
+++ schema_mapping/schema_test.go
@@ -12,6 +12,21 @@ var testcases = []struct {
 	{src: Schema{[]Field{{"country", []string{"cnt", "countryLong"}}}},
 		target:   Schema{[]Field{{"countryLong", []string{"country"}}}},
 		expected: map[string]string{"country": "countryLong"}},
+	{src: Schema{[]Field{
+		{"country", []string{"nationality"}},
+		{"state", []string{"st", "territory"}},
+		{"city", []string{"cty", "burrow"}},
+	}},
+		target: Schema{[]Field{
+			{"st", []string{"not country", "state", "sttt"}},
+			{"burrow", []string{"hometown", "city"}},
+			{"nationality", []string{"country", "nation", "residency"}},
+		}},
+		expected: map[string]string{
+			"country": "nationality",
+			"state":   "st",
+			"city":    "burrow",
+		}},
 }
 
 func TestMapping(t *testing.T) {
